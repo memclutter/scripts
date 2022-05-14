049a7b3edd117c07d47e65456242290799f707ae
Memory Clutter <memclutter@gmail.com>
2022-05-14 10:53:21 +0300

    Added gitlab variables command structure

--- gitlab/variables.py
+++ gitlab/variables.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 """
 This script is used to import/export ci/cd variables. This is used in situations when moving a project.
+Command structure:
+- `./variables.py export` print ci/cd variables to stdout
+- `./variables.py import` send environment variables from stdin
+- `./variables.py export | ./variables.py import` transfer variables
 """
 import sys
 
