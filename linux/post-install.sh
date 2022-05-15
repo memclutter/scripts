3c05199ed24a2962daa054b9bfb38adec2965baf
Memory Clutter <memclutter@gmail.com>
2022-05-15 23:46:26 +0300

    Added function for update system and install requirements

--- linux/post-install.sh
+++ linux/post-install.sh
@@ -11,3 +11,20 @@
 # - Install docker-compose? [yes]
 # - Install zsh [yes]
 # - Install oh-my-zsh [no]
+OS_RELEASE=`awk -F= '/^ID/{print $2}' /etc/os-release`
+
+log () {
+  echo "[$1]: $2"
+}
+
+install_required_packages () {
+  log INFO "install required packages"
+  if [ "$OS_RELEASE" == "ubuntu" ] || [ "$OS_RELEASE" == "debian" ]; then
+    log DEBUG "apt install git curl vim"
+    apt update -qqy && apt upgrade -qqy && apt install -qqy git curl vim
+  fi
+}
+
+log INFO "detect os release ${OS_RELEASE}"
+
+install_required_packages
